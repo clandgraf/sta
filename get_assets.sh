e72166a2017a66f0b534990930c04eba1c2ea8e7
Christoph Landgraf <christoph.landgraf@googlemail.com>
2020-05-24 18:24:15 +0200

    housekeeping

---
+++ get_assets.sh
@@ -0,0 +1,3 @@
+mkdir -p assets
+wget -P assets https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Medium.ttf
+wget -P assets https://raw.githubusercontent.com/adobe-fonts/source-sans-pro/release/TTF/SourceSansPro-Regular.ttf
