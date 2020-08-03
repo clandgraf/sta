9f143173ae7def3fba3df1ff89ff016e467cecf3
Christoph Landgraf <christoph.landgraf@googlemail.com>
2020-08-03 17:54:44 +0200

    GUI Refactoring

---
+++ src/gui/gui_filebrowser.cpp
@@ -0,0 +1,88 @@
+#include <imgui.h>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include "gui_filebrowser.hpp"
+#include "util.hpp"
+
+static const int BUFFER_LEN = 0xff;
+
+namespace fs = std::filesystem;
+static fs::path fileBrowserCurrentDir;
+static char fileBrowserInputBuffer[BUFFER_LEN];
+static std::vector<fs::path> files;
+static std::vector<const char *> filesCstr;
+static int fileBrowserSelection = 0;
+
+static void updateChildren() {
+    strncpy(fileBrowserInputBuffer, fileBrowserCurrentDir.string().c_str(), BUFFER_LEN - 1);
+
+    for (const auto& cstr : filesCstr) {
+        free((void*)cstr);
+    }
+    filesCstr.clear();
+    files.clear();
+    for (const auto& entry : fs::directory_iterator(fileBrowserCurrentDir)) {
+        files.push_back(entry.path());
+    }
+
+    for (const auto& entry : files) {
+        filesCstr.push_back(_strdup(entry.filename().string().c_str()));
+    }
+}
+
+void updateFromPath(const fs::path& p) {
+    fileBrowserCurrentDir = p;
+    updateChildren();
+}
+
+void updateFromString(const std::string& p) {
+    fileBrowserCurrentDir = p;
+    updateChildren();
+}
+
+bool ImGui_FileBrowser(fs::path& selectedFile) {
+    if (ImGui::Button("Up")) {
+        updateFromPath(fileBrowserCurrentDir.parent_path());
+    }
+    ImGui::SameLine(); 
+    if (ImGui::InputText("##Path", fileBrowserInputBuffer, IM_ARRAYSIZE(fileBrowserInputBuffer), ImGuiInputTextFlags_EnterReturnsTrue)) {
+        fs::path tmpPath = fileBrowserInputBuffer;
+        if (fs::exists(tmpPath)) {
+            if (fs::is_directory(tmpPath)) {
+                updateFromPath(tmpPath);
+            } else {
+                selectedFile = tmpPath;
+                Settings::set("rom-directory", selectedFile.parent_path());
+                return true;
+            }
+        } else {
+            updateFromPath(fileBrowserCurrentDir);
+        }
+    }
+
+    ImGui::PushItemWidth(-1);
+    if (ImGui::ListBox("##Files", &fileBrowserSelection, filesCstr.data(), (int)filesCstr.size(), 10)) {
+        fs::path selectedPath = files[fileBrowserSelection];
+        if (fs::is_directory(selectedPath)) {
+            updateFromPath(selectedPath);
+        } else {
+            selectedFile = selectedPath;
+            Settings::set("rom-directory", selectedFile.parent_path().string());
+            return true;
+        }
+    }
+    ImGui::PopItemWidth();
+
+    return false;
+}
+
+void ImGui_FileBrowser_Init() {
+#if defined(_WIN32) || defined(WIN32) 
+    std::string home{getenv("USERPROFILE")};
+#else
+    std::string home{getenv("HOME")};
+#endif
+
+    updateFromString(Settings::get("rom-directory", home));
+}
---
+++ src/gui/gui_filebrowser.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <filesystem>
+
+extern bool ImGui_FileBrowser(std::filesystem::path&);
+extern void ImGui_FileBrowser_Init();
